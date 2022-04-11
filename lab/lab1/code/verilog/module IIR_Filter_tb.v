e7bfc06df144a629d645dea9c0595326228b1b03
Edith <edith_lzh@sjtu.edu.cn>
2022-04-11 11:12:08 +0800

    code update

--- lab/lab1/code/verilog/module IIR_Filter_tb.v
+++ lab/lab1/code/verilog/module IIR_Filter_tb.v
@@ -1,24 +1,23 @@
 `timescale 1 ns/ 1 ns
-module IIR_filter_tst();
+module filter_original_test();
     
     parameter clk_period      = 200; //����ʱ���ź����ڣ�Ƶ�ʣ�:1.6MHz
     parameter clk_half_period = clk_period/2;
-    parameter Data_num        = 2000;  //�������ݳ���
+    parameter data_num        = 2000;  //�������ݳ���
     
     reg clk;
     reg rst;
-    reg [7:0] Data_in;
-    wire [17:0] Data_out;
-    reg[7:0] in[Data_num:0];
+    reg [7:0] data_in;
+    wire [18:0] data_out;
+    reg[7:0] in[data_num:0];
     integer i, fp_w;
-    reg[17:0] out[Data_num:0];
+    reg[18:0] out[data_num:0];
     
-    IIR_Filter_8 A1 (
-    .Data_out(Data_out),
-    .Data_in(Data_in),
+    filter_original u(
     .clk(clk),
-    .rst(rst)
-    );
+    .rst(rst),
+    .data_in(data_in),
+    .data_out(data_out));
     
     initial
     begin
@@ -35,10 +34,10 @@ module IIR_filter_tst();
     
     always @(posedge clk)
     begin
-        Data_in <= in[i];
-        out[i]  <= Data_out;
-        $fwrite(fp_w, "%b\n", Data_out);//write
+        data_in <= in[i];
+        out[i]  <= data_out;
+        $fwrite(fp_w, "%b\n", data_out);//write
         i     = i+1;
-        if (i == Data_num+1) $fclose(fp_w);
+        if (i == data_num+1) $fclose(fp_w);
     end
 endmodule
